7deb3e22ba42a8050d0aec4728eaf5ea05e55635
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-10-13 00:27:22 +0530

    Unused Folder mongo

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,37 +1,32 @@
 import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
-import {config} from "dotenv";
+import { config } from "dotenv";
 
 export async function GET(request) {
-
-
   const uri = process.env.MONGODB_URI;
   const client = new MongoClient(uri);
   try {
-    const database = client.db('stock');
-    const inventory = database.collection('inventory');
+    const database = client.db("stock");
+    const inventory = database.collection("inventory");
     const query = {};
     const products = await inventory.find(query).toArray();
-    return NextResponse.json({ success: true, products })
+    return NextResponse.json({ success: true, products });
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
-
 }
 
 export async function POST(request) {
-  // Replace the uri string with your connection string.
-  let body = await request.json()
+  let body = await request.json();
   const uri = process.env.MONGODB_URI;
   const client = new MongoClient(uri);
   try {
-    const database = client.db('stock');
-    const inventory = database.collection('inventory');
-    const product = await inventory.insertOne(body)
-    return NextResponse.json({ product, ok: true })
+    const database = client.db("stock");
+    const inventory = database.collection("inventory");
+    const product = await inventory.insertOne(body);
+    return NextResponse.json({ product, ok: true });
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
